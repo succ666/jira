e76ca5af60204fb0506cd2293dae3b99b69216e2
succ <2014649323@qq.cm>
2023-08-08 14:33:29 +0800

    增加dev-tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-import {ProjectListScreen} from "./screens/project-lilst";
-import {TsReactTest} from "./screens/try-use-array";
 import {LoginScreen} from "./screens/login";
 
 function App() {
   return (
     <div className="App">
-      {/*<ProjectListScreen/>*/}
-      {/*  <TsReactTest></TsReactTest>*/}
         <LoginScreen></LoginScreen>
     </div>
   );
