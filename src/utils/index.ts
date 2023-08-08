f0cc124f2ab99b948b79aa07686868b2047e7466
succ <2014649323@qq.cm>
2023-08-08 10:28:34 +0800

    给useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 
-export const isFalsy = (value) => value === 0 ? false : !value;
-export const cleanObject = (object) => {
+export const isFalsy = (value: any) => value === 0 ? false : !value;
+export const cleanObject = (object: any) => {
     const result = {...object}
     Object.keys(result).forEach(key => {
         const value = result[key]
@@ -11,18 +11,18 @@ export const cleanObject = (object) => {
     })
     return result
 }
-export const useMount = (callback) => {
+export const useMount = (callback: any) => {
     useEffect( () => {
         callback()
     }, [])
 }
 
-export const useDebounce = (value, delay) => {
-    const [debouncedValue, setDebouncedValue] = useState(value)
+export const useDebounce = <T>(value: T, delay?: number) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
     useEffect(() => {
-        const timeout = setTimeout(() => {setDebouncedValue(value)}, delay)
+        const timeout = setTimeout(()=> setDebouncedValue(value), delay)
         // 每次在上一个useEffect处理完以后再运行
         return () => clearTimeout(timeout)
     },[value, delay])
-    return debouncedValue
-}
+    return debouncedValue;
+};
