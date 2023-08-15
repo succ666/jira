717a7e2e036efbfd9abcefae6ecd2a87cb563331
succ666 <2014649323@qq.cm>
2023-08-15 11:27:23 +0800

    优化样式

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,10 +2,33 @@ import React from "react";
 import {ProjectListScreen} from "./screens/project-lilst";
 import {useAuth} from "./auth-context";
 import {Button} from "antd";
+import styled from "@emotion/styled";
+import {Row} from "./components/lib";
 export const AuthenticatedApp = () => {
     const {logout} = useAuth()
     return <div>
-        <Button onClick={logout}>登出</Button>
-        <ProjectListScreen/>
+        <Header between={true}>
+            <HeaderLeft gap={true}>
+                <h2>Logo</h2>
+                <h2>项目</h2>
+                <h2>用户</h2>
+            </HeaderLeft>
+            <HeaderRight>
+                <Button onClick={logout}>登出</Button>
+            </HeaderRight>
+        </Header>
+        <Main>
+            <ProjectListScreen/>
+        </Main>
     </div>
 }
+
+const Header = styled(Row)`
+  height: 6rem;
+`
+const HeaderLeft = styled(Row)`
+`
+const HeaderRight = styled.div``
+const Main = styled.main`
+height: calc(100vh - 6rem);
+`
